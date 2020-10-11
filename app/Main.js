6d19faba1912a5389174992b1855b3547698328a
“Saurja” <saurjaghoshofficial@gmail.com>
2020-10-11 09:45:55 +0530

    Added Routing

--- app/Main.js
+++ app/Main.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //  My Components
 import Header from './components/Header.js'
 import HomeGuest from './components/HomeGuest.js'
 import Footer from './components/Footer.js'
+import About from './components/About'
+import Terms from './components/Terms'
 
 function Main () {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-
-            <HomeGuest />
-
+            <Switch>
+                <Route path="/" exact><HomeGuest/></Route>
+                <Route path="/about-us" exact><About/></Route>   
+                <Route path="/terms" exact><Terms/></Route> 
+            </Switch>
             <Footer />
-
-        </>
+        </BrowserRouter>
     )
 }
 
